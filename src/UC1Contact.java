927a39e25479e40c725477b40b382a65bca28ed8
sachinbidkikar <sachinbidkikar@gmail.com>
2023-01-25 18:43:44 +0530

    UC1

---
+++ src/UC1Contact.java
@@ -0,0 +1,22 @@
+public class UC1Contact {
+    private String fName;
+    private String lName;
+    private String add;
+    private String city;
+    private String state;
+    private int pinCode;
+    private long phoneNum;
+    private String email;
+
+    public UC1Contact(String fName, String lName, String add, String city, String state, int pinCode, long phoneNum, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.add = add;
+        this.city = city;
+        this.state = state;
+        this.pinCode = pinCode;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+
+}
